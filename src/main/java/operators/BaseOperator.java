8737ca480d0562902e0fcff0474b5ceff80a613d
JerryW35 <wzr@bu.edu>
2023-04-17 22:52:51 -0400

    fix data source time stamp

--- src/main/java/operators/BaseOperator.java
+++ src/main/java/operators/BaseOperator.java
@@ -31,7 +31,7 @@ public abstract class BaseOperator extends Thread implements Serializable, IKeyG
 
     private IKeySelector keySelector = new DefaultKeySelector();
     protected Tm.Msg currentInputMsg;
-    protected long startTimeStamp = (long) (System.currentTimeMillis() / 1000.0);
+    protected  static long  startTimeStamp;
     protected boolean firstElementFlag = true;
     protected boolean sendWatermarkOrNot = true;
     //key: OperatorName, value: MaxWatermark. Use this to calculate min of the max watermark
